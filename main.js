1385081319bb19d0b0adefffb8a44ccd065e5636
CritianRodrigo <128195267+CritianRodrigo@users.noreply.github.com>
2023-10-20 20:14:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+// Pom ----------------------------------------
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom');
+document.querySelectorAll('.teclas')
